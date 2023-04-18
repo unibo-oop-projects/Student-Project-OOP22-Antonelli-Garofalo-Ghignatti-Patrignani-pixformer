baef3bb7f4dbc755e906d60007892b2df6c9485f
Roberto Casadei <robyonrails@gmail.com>
2023-04-18 12:40:38 +0200

    oop machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
-rootProject.name = "pixformer"
+plugins {
+    id("com.gradle.enterprise") version "3.13"
+}
 
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
+
+rootProject.name = "pixformer"
